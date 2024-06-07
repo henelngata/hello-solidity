2459aececf42f71d8b441cbfee32fbb5f70e3c61
henelngata <bucketpena013@gmail.com>
2024-06-07 04:47:02 -0700

    all

---
+++ contracts/HelloSolidity.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+contract HelloWorld{
+
+// this is a single line comment
+/*
+    a multiple line comment
+*/
+    // state variables are stored permanently
+
+    /*Booleans*/
+    bool isNamed = true; // bool either true or false
+
+    /*Integers*/
+    
+}
